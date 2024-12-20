9f1ca6f6d30d2042b633b954cb551ab85b4cdb5f
hazra <hazra@aufaitux.com>
2024-12-20 17:02:58 +0530

    feat:

---
+++ assets/js/custom/owl-carousel.js
@@ -0,0 +1,114 @@
+$(document).ready(function () {
+  var owl = $("#project-card-wrapper").owlCarousel({
+    loop: false,
+    margin: 32,
+    nav: true,
+    dots: true,
+    responsiveClass: true,
+    stagePadding: 0,
+    responsive: {
+      0: {
+        items: 1,
+        stagePadding: 30,
+        margin: 16,
+      },
+      568: {
+        items: 1,
+        stagePadding: 50,
+        margin: 16,
+      },
+      768: {
+        stagePadding: 60,
+        items: 1,
+        margin: 16,
+      },
+      1200: {
+        stagePadding: 60,
+        items: 1,
+      },
+      1400: {
+        stagePadding: 110,
+        items: 1,
+      }
+    },
+    onInitialized: adjustDots, // Custom function to adjust dots on initialization
+    onChanged: adjustDots      // Custom function to adjust dots when slides change
+  });
+
+  function adjustDots(event) {
+    // Remove small-dot class from all dots initially
+    $(".owl-dot").removeClass('small-dot');
+
+    // Check if the carousel is at the last item
+    var items = event.item.count;
+    var last = event.item.index + event.page.size >= items;
+
+    if (last) {
+      // Add 'small-dot' class to the first dot if on last item
+      $(".owl-dot").first().addClass('small-dot');
+    } else {
+      // Add 'small-dot' class to the last dot if not on last item
+      $(".owl-dot").last().addClass('small-dot');
+    }
+  }
+});
+
+
+$(document).ready(function () {
+  var owl = $("#testimonial-carousel").owlCarousel({
+    loop: false,
+    margin: 32,
+    nav: true,
+    dots: true,
+    responsiveClass: true,
+    stagePadding: 0,
+    responsive: {
+      0: {
+        items: 1,
+        // margin: 16,
+        stagePadding: 20,
+      },
+      568: {
+        items: 1,
+        // margin: 16,
+        stagePadding: 20,
+      },
+      768: {
+        stagePadding: 40,
+        items: 1,
+        margin: 16,
+      },
+      1200: {
+        stagePadding: 60,
+        items: 1,
+      },
+      1400: {
+        stagePadding: 65,
+        items: 1,
+      }
+    },
+    onInitialized: adjustDots, // Custom function to adjust dots on initialization
+    onChanged: adjustDots      // Custom function to adjust dots when slides change
+  });
+
+  function adjustDots(event) {
+    // Remove small-dot class from all dots initially
+    $(".owl-dot").removeClass('small-dot');
+
+    // Check if the carousel is at the last item
+    var items = event.item.count;
+    var last = event.item.index + event.page.size >= items;
+
+    if (last) {
+      // Add 'small-dot' class to the first dot if on last item
+      $(".owl-dot").first().addClass('small-dot');
+    } else {
+      // Add 'small-dot' class to the last dot if not on last item
+      $(".owl-dot").last().addClass('small-dot');
+    }
+  }
+});
+
+
+
+
